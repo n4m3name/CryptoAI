5226c45bfa1db0ab1fd0db95de7a03ce3e251f87
jojohn01 <jojohnofuya@gmail.com>
2025-02-25 13:41:22 -0800

    Starting preprocessing

---
+++ preprocessor.py
@@ -0,0 +1,21 @@
+
+import csv
+from collections import deque
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+
+def get_last_lines(name, n=5000): #john
+    with open(name) as f:
+        lines = deque(csv.reader(f), maxlen=n)
+    return np.array(lines, dtype=float)
+
+def time_to_row(time): #john
+    date
+
+def convert_date(array): #john
+    times = array[:, -1]
+    new_arr = np.array
+
+get_last_lines("../Kraken_OHLCVT/XBTUSD_15.csv")
